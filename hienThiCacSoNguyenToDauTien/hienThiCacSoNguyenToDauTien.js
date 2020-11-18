c2ab0395bcbc4f555648bd6632c37e8ffbc36957
gio lang du <you@example.com>
2020-11-18 10:50:05 +0700

    update finished

--- hienThiCacSoNguyenToDauTien/hienThiCacSoNguyenToDauTien.js
+++ hienThiCacSoNguyenToDauTien/hienThiCacSoNguyenToDauTien.js
@@ -1,31 +1,18 @@
-function isPrime() {
-    let num = 1;
-    let count = 0;
-    for (count = 0; count < 20; ) {
-
-        if (num === 2) {
-            num++;
-            count++;
-        }
-        else
-            if (num > 1) {
-                for (var i = 2; i < Math.sqrt(num); i++) {
-
-                    if (num % i !== 0) {
-                        num++;
-                        count++;
-                    } else if (num === i * i) {
-                        num++
-                    } else {
-                        num++;
-                    }
-                }
-            } else {
-                num++;
-            }
-            document.write(num);
-        }
-
-
+function isPrime(n) {
+    if (n<2) return false;
+    for (let i=2;i<=Math.sqrt(n);i++) {
+        if (n%i==0) return false;
+    }
+    return true;
 }
-
+function countPrime () {
+    let count;
+    let n=1;
+    let m = +prompt('Ban muon hien thi bao nhieu so nguyen to dau tien?')
+    for (count=0; count<m;n++) {
+        if (isPrime(n)) {
+            count++
+            console.log(n);
+        }
+    }
+}
